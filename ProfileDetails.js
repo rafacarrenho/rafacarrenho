36b6e4ee133058069713051567328328b40a52f8
Rafael Carrenho <rafael.carrenho@hotmail.com>
2021-07-10 21:04:23 -0300

    Feat: Profile Details

---
+++ ProfileDetails.js
@@ -0,0 +1,17 @@
+const ProfileDetails = {
+  fullName: 'Rafael Barbosa Carrenho',
+  skills: {
+    languages: ['JavaScript', 'TypeScript', 'HTML', 'CSS/SASS'],
+    frameworks: ['ReactJS', 'NextJS', 'React Native', 'VueJS', 'NodeJS'],
+    tests: ['Jest', 'Testing LIbrary', 'Cypress'],
+    tools: ['Git', 'Github', 'GitLab', 'Code Commit', 'Amplify', 'Vercel'],
+    OS: ['Windows', 'Linux', 'MacOS', 'Android', 'IOS'],
+  },
+  socials: {
+    linkedIn: 'https://www.linkedin.com/in/rafael-carrenho/',
+    github: 'https://github.com/rafacarrenho',
+    website: 'https://rcarrenho.com.br/',
+    behance: 'https://www.behance.net/rafaelcarrenho',
+    email: 'rafael.carrenho@hotmail.com',
+  },
+};
